ffcbf75028a0caaad08b96db5c35eac12b6b22ec
João Afonso <joaoafonso386@gmail.com>
2021-11-02 15:38:33 +0000

    Added basic routing setup

--- frontend/src/components/User/User.js
+++ frontend/src/components/User/User.js
@@ -7,31 +7,27 @@ import UserForm from "./UserForm";
 
 
 const User = () => {
-        
-        const [edit, setEdit] = useState(false);
+  const [edit, setEdit] = useState(false);
 
-        return (
-            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-                
-                
-                    <div>
-                <h3>Username</h3>
-                <h3>password</h3>
-                <h3>Age</h3>
-                
-                
-                    </div>
-                    {edit ? (
-                    <UserForm />
-                    ) : (
-                    null )}
-                <div>
-                    
-                </div>
-                
-                <button onClick={setEdit(false)}>edit</button>
-            </div>
-        )
-}
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      <div>
+        <h3>Username</h3>
+        <h3>password</h3>
+        <h3>Age</h3>
+      </div>
+      {edit ? <UserForm /> : null}
+      <div></div>
+
+      <button onClick={() => setEdit(false)}>edit</button>
+    </div>
+  );
+};
 
 export default User;
