426fa30c8bda69ee193e36b6bdeacef4df15d0fd
João Afonso <joaoafonso386@gmail.com>
2021-11-02 18:21:28 +0000

    Last changes merge

--- frontend/src/components/Invoices/Invoice.js
+++ frontend/src/components/Invoices/Invoice.js
@@ -6,7 +6,12 @@ const Invoice = () => {
   return (
     <div>
       <div>
-        <form></form>
+        <form>
+          <label>
+            Invoice Number
+            <input type="number" />
+          </label>
+        </form>
       </div>
       <div>//Invoice List component</div>
     </div>
