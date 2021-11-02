18f085bd55c8abd97ff042fa31d4960c8bc8422c
João Afonso <joaoafonso386@gmail.com>
2021-11-02 14:24:40 +0000

    Added axios and fetched users from endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import Login from "./components/Login/Login";
 import "./App.css";
 
 function App() {
+  //Definir um state com um user para depois passar para a aplicação
+
+  //Definir um state (boolean) para saber se o utilizador esta logado ou não
+
   return (
     <div className="App">
       <Login />
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const url = "https://6181431832c9e2001780472a.mockapi.io/username";
+
+  const getUser = (e) => {
+    e.preventDefault();
+    //fazer request ao endpoint para receber um user
+    axios.get(url).then((response) => {
+      console.log(response.data);
+    });
+  };
+
   return (
     <div>
-      <form>
-        <label for="username">Username</label>
-        <input id="username" name="username" type="text" />
+      <form onSubmit={getUser}>
+        <label htmlFor="username">Username</label>
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          id="username"
+          name="username"
+          type="text"
+        />
         <label for="username">Password</label>
-        <input id="username" name="username" type="password" />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          id="username"
+          name="username"
+          type="password"
+        />
         <button type="submit">Login</button>
       </form>
     </div>
