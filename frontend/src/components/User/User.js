a62416edd313ae3749eff2cc1a9b5de681c7057d
rafael <rafael.bravo.costa@gmail.com>
2021-11-02 15:16:00 +0000

    user form edit

--- frontend/src/components/User/User.js
+++ frontend/src/components/User/User.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
+import { useState } from "react";
+import UserForm from "./UserForm";
 
 
 
 const User = () => {
-
+        
+        const [edit, setEdit] = useState(false);
 
         return (
             <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
@@ -21,31 +21,15 @@ const User = () => {
                 
                 
                     </div>
+                    {edit ? (
+                    <UserForm />
+                    ) : (
+                    null )}
                 <div>
-                    <Form>
-                                <Form.Group className="Username" controlId="formUser">
-                                    <Form.Label>Username</Form.Label>
-                                    <Form.Control placeholder="your username"/>
-                                </Form.Group>
-
-                                <Form.Group className="password" controlId="formUser">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control placeholder="your password"/>
-                                </Form.Group>
-
-                                <Form.Group className="Age" controlId="formUser">
-                                    <Form.Label>Age</Form.Label>
-                                    <Form.Control placeholder="your Age"/>
-                                </Form.Group>
-
-
-                    <Button variant="primary" type="submit" className="submitButton">
-                Submit
-                </Button>
-                    </Form>
+                    
                 </div>
                 
-                <button>edit</button>
+                <button onClick={setEdit(false)}>edit</button>
             </div>
         )
 }
