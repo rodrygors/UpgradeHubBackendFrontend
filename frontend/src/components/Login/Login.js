ab9205196703e5c49ce5b8d4aef2e54139a26274
João Afonso <joaoafonso386@gmail.com>
2021-11-02 16:16:51 +0000

    Added CSS to the login

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import "./Login.css";
+
 const Login = ({ setIsLogged, setUser }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -29,26 +31,61 @@ const Login = ({ setIsLogged, setUser }) => {
 
   return (
     <div>
-      {failedLoggin ? <p>Username or password are incorrect!</p> : ""}
-      <form onSubmit={getUser}>
-        <label htmlFor="username">Username</label>
-        <input
-          onChange={(e) => setUsername(e.target.value)}
-          id="username"
-          name="username"
-          type="text"
-        />
-        <label for="username">Password</label>
-        <input
-          onChange={(e) => setPassword(e.target.value)}
-          id="username"
-          name="username"
-          type="password"
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <div className="container">
+		<div className="container-login">
+    <div className="wrap-login">
+    <form  onSubmit={getUser} className="login-form">
+    <span className="login-form-title">
+    Faça o Login
+    </span>
+    {failedLoggin ? <p className="alert">Username or password are incorrect!</p> : ""}
+
+					<div className="wrap-input margin-top-35 margin-bottom-35">
+						<input onChange={(e) => setUsername(e.target.value)} className="input-form" type="text" name="username" id="username" placeholder="Username" autocomplete="off"/>
+						<span className="focus-input-form"></span>
+					</div>
+
+					<div className="wrap-input margin-bottom-35">
+						<input onChange={(e) => setPassword(e.target.value)} className="input-form" type="password" name="password" placeholder="Password"/>
+						<span className="focus-input-form"></span>
+					</div>
+
+					<div className="container-login-form-btn">
+						<button type="submit" className="login-form-btn">
+							Login
+						</button>
+					</div>
+
+				</form>
+			</div>
+			
+		</div>
+	</div>
+  </div>
+    
   );
 };
 
 export default Login;
+
+
+// <div>
+//       {failedLoggin ? <p>Username or password are incorrect!</p> : ""}
+//       <form onSubmit={getUser}>
+//         <label htmlFor="username">Username</label>
+//         <input
+//           onChange={(e) => setUsername(e.target.value)}
+//           id="username"
+//           name="username"
+//           type="text"
+//         />
+//         <label for="username">Password</label>
+//         <input
+//           onChange={(e) => setPassword(e.target.value)}
+//           id="username"
+//           name="username"
+//           type="password"
+//         />
+//         <button type="submit">Login</button>
+//       </form>
+//     </div>
