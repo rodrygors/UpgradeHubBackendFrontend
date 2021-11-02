35408fffff8f4f5edea4a0f421bceb70099bfbf8
rafael <rafael.bravo.costa@gmail.com>
2021-11-02 14:53:14 +0000

    User form and rows

--- frontend/src/components/User/User.js
+++ frontend/src/components/User/User.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
 
 
@@ -8,20 +11,40 @@ const User = () => {
 
 
         return (
-            <div>
-
-                <Row>
-                    <Col>
+            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                
+                
+                    <div>
                 <h3>Username</h3>
                 <h3>password</h3>
                 <h3>Age</h3>
                 
                 
-                </Col>
-                <Col>
+                    </div>
+                <div>
+                    <Form>
+                                <Form.Group className="Username" controlId="formUser">
+                                    <Form.Label>Username</Form.Label>
+                                    <Form.Control placeholder="your username"/>
+                                </Form.Group>
+
+                                <Form.Group className="password" controlId="formUser">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control placeholder="your password"/>
+                                </Form.Group>
+
+                                <Form.Group className="Age" controlId="formUser">
+                                    <Form.Label>Age</Form.Label>
+                                    <Form.Control placeholder="your Age"/>
+                                </Form.Group>
+
+
+                    <Button variant="primary" type="submit" className="submitButton">
+                Submit
+                </Button>
+                    </Form>
+                </div>
                 
-                </Col>
-                </Row>
                 <button>edit</button>
             </div>
         )
