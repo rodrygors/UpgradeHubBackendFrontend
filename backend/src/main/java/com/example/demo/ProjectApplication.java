ceb3f57af72a37a3caef1f67b1412de5fa0db314
Rodrigo Ramos da Silva <a35479@alunos.ipb.pt>
2021-11-03 16:52:07 +0000

    cors

--- backend/src/main/java/com/example/demo/ProjectApplication.java
+++ backend/src/main/java/com/example/demo/ProjectApplication.java
@@ -2,15 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
 @SpringBootApplication
-@EnableWebSecurity
 public class ProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
 
 }
