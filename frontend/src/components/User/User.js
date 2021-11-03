5ddce36cba1b9c4ce0c6b8ef7264eadee29a51ac
rafael <rafael.bravo.costa@gmail.com>
2021-11-03 13:00:26 +0000

    user working

--- frontend/src/components/User/User.js
+++ frontend/src/components/User/User.js
@@ -1,32 +1,46 @@
 import React from "react";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useState, useEffect } from "react";
 import UserForm from "./UserForm";
 import axios from "axios";
-
-
 
 const User = ({ user }) => {
   const [edit, setEdit] = useState(false);
 
-  const [change, setChange] = useState({user});
+  const [change, setChange] = useState();
 
-  const res = axios.put('http://localhost:8080/users/' + user.id, { age: '5', name: 'string', password: 'dada' });
+  const [userup, setUserup] = useState();
 
+  // const res = axios.put('http://localhost:8080/users/' + user.id, {
+  //   age: '5',
+  //   name: 'string',
+  //   password: 'dada' }
+  //   );
 
-  /*handleChange (event) {
-    setChange({value: event.target.value});
+  const handleChange = (event) => {
+    setChange({ value: event.target.value });
+  };
+
+  function handleSubmit() {
+    console.log(change);
+    console.log(change.name);
+    axios.put("http://localhost:8080/user/" + change.id, {
+        age: change.age,
+        name: change.name,
+        password: change.password,
+    });
+    alert("A change was made");
   }
 
-  handleSubmit(event) {
-    alert('A change was made');
-    event.preventDefault();
-  }
-  */
+  useEffect(() => {
+    setChange(user);
+  }, [user]);
+
+  //  console.log(user)
+  console.log(change);
 
   return (
-
     <div
       style={{
         display: "flex",
@@ -46,30 +60,43 @@ const User = ({ user }) => {
         )}
       </div>
 
-      <div>{edit ? 
       <div>
+        {edit ? (
+          <div>
+            <form onSubmit={() => handleSubmit(change)}>
+              <label>
+                Name:
+                {/* <input type="text" value={user.name} onChange={()=> setChange(change.name)} /> */}
+                <input
+                  type="text"
+                  value={change.name}
+                  onChange={(e) => {setChange({...change, name:e.target.value});
+                  }}
+                />
+              </label>
 
-      {/* <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={change.name} onChange={handleChange} />
-        </label>
-      
-        <label>
-          password:
-          <input type="text" value={change.password} onChange={handleChange} />
-        </label>
+              <label>
+                password:
+                <input
+                  type="text"
+                  value={change.password}
+                  onChange={(e) => setChange(e.target.value)}
+                />
+              </label>
 
-        <label>
-          age:
-          <input type="text" value={change.age} onChange={handleChange} />
-        </label>
-        <button type="submit" value="Submit" />
-
-      </form> */}
-
-    </div>
-     : null}</div>
+              <label>
+                age:
+                <input
+                  type="number"
+                  value={change.age}
+                  onChange={(e) => setChange(change.age)}
+                />
+              </label>
+              <button type="submit" value="Submit" />
+            </form>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
